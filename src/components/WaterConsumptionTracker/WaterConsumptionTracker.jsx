06f666231ca1e0d7df2c37134c6ab5109a8160fa
Your NameNastya-20 <asyataa2016@gmail.com>
2024-12-13 21:54:38 +0200

    add

--- src/components/WaterConsumptionTracker/WaterConsumptionTracker.jsx
+++ src/components/WaterConsumptionTracker/WaterConsumptionTracker.jsx
@@ -13,19 +13,19 @@ const WaterConsumptionTracker = () => {
         <div className={css.trackerBenefits}>
         <div>
           <svg className={css.icon}>
-            <use href="/assets/welcomeIcons.svg#icon-habit" />
+            <use href="./assets/welcomeIcons.svg#icon-habit" />
           </svg>
           <span>Habit drive</span>
         </div>
         <div>
           <svg className={css.icon}>
-            <use href="/assets/welcomeIcons.svg#icon-statistics" />
+            <use href="./assets/welcomeIcons.svg#icon-statistics" />
           </svg>
           <span>View statistics</span>
         </div>
         <div>
           <svg className={css.icon}>
-            <use href="/assets/welcomeIcons.svg#icon-settings" />
+            <use href="./assets/welcomeIcons.svg#icon-settings" />
           </svg>
           <span>Personal rate setting</span>
         </div>
