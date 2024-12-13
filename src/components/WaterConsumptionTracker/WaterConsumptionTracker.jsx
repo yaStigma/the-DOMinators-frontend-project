37cabad23bc7ba5370ffcac91a3726ef8c1f0f46
Your NameNastya-20 <asyataa2016@gmail.com>
2024-12-13 18:20:26 +0200

    add welcome-page

---
+++ src/components/WaterConsumptionTracker/WaterConsumptionTracker.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import css from "./WaterConsumptionTracker.module.css";
+
+const WaterConsumptionTracker = () => {
+  return (
+    <div className={css.waterTracker}>
+      <h1>Water consumption tracker</h1>
+      <p>Record daily water intake and track</p>
+         <h2>Tracker Benefits</h2>
+        <div className={css.trackerBenefits}>
+        <div>
+          <svg className={css.icon}>
+            <use href="/assets/weicomeIcons.svg#icon-habit" />
+          </svg>
+          <span>Habit drive</span>
+        </div>
+        <div>
+          <svg className={css.icon}>
+            <use href="/assets/weicomeIcons.svg#icon-statistics" />
+          </svg>
+          <span>View statistics</span>
+        </div>
+        <div>
+          <svg className={css.icon}>
+            <use href="/assets/weicomeIcons.svg#icon-settings" />
+          </svg>
+          <span>Personal rate setting</span>
+        </div>
+      </div>
+      <button onClick={() => window.location.href = "/signup"}>
+        Try tracker
+      </button>
+    </div>
+  );
+};
+
+export default WaterConsumptionTracker;
