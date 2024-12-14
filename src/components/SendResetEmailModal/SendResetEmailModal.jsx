efe7add4323f6bf617473c9951dbdd895acb1385
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-12-14 21:20:46 +0100

    styling of modal window finished

--- src/components/SendResetEmailModal/SendResetEmailModal.jsx
+++ src/components/SendResetEmailModal/SendResetEmailModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { AuthForm } from '../AuthForm/AuthForm';
 import * as Yup from 'yup'; 
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useDispatch } from 'react-redux'; 
 import { sendResetPasswordEmail } from '../../redux/auth/operations';
 import css from "./SendResetEmailModal.module.css";
@@ -8,21 +8,20 @@ import css from "./SendResetEmailModal.module.css";
 export const SendResetEmailModal = ({ onClose }) => {
   const dispatch = useDispatch();
 
-  // Поля формы
-  const fields = [
-    {
-      name: "email",
-      type: "email",
-      label: "Email",
-      placeholder: "Enter your email",
-      validation: Yup.string()
-        .email("Invalid email address")
-        .required("Email is required"),
-    }
-  ];
 
-  // Обработчик отправки формы
-  const handleSendResetEmail = async (values) => {
+  const initialValues = {   
+    email: "",
+  };
+
+ 
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+  });
+
+
+  const handleSendResetEmail = async (values, { setSubmitting }) => {
     try {
       await dispatch(sendResetPasswordEmail(values.email)); 
       alert("Password reset email sent!");
@@ -30,21 +29,46 @@ export const SendResetEmailModal = ({ onClose }) => {
     } catch (error) {
       console.error("Error sending reset email:", error);
       alert("Something went wrong. Please try again.");
+    } finally {
+      setSubmitting(false);
     }
   };
 
   return (
     <div className={css.overlay} onClick={onClose}>
       <div className={css.modal} onClick={(e) => e.stopPropagation()}>
-        <h2 className={css.title}>Reset your Password</h2>
-        <AuthForm
-          fields={fields}
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
           onSubmit={handleSendResetEmail}
-          className={css.formContainer}
-        />
+        >
+          {({ isSubmitting }) => (
+            <Form className={css.formContainer} autoComplete="off">
+            <h2 className={css.title}>Reset your Password</h2>
+              <label className={css.formLabel}>
+                Email
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  className={css.formField}
+                />
+                <ErrorMessage name="email" component="div" className={css.error} />
+              </label>
+
+              <button 
+                type="submit" 
+                disabled={isSubmitting} 
+                className={css.btn}
+              >
+                {isSubmitting ? "Reseting Password..." : "Reset Password"}
+              </button>
+            </Form>
+          )}
+        </Formik>
         <button onClick={onClose} className={css.closeButton}>Close</button>
       </div>
     </div>
   );
 };
-
