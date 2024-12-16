a1361ea6456a2cecb7ccdd00aef86c185b4a06ac
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-12-16 17:35:48 +0100

    toster deleted

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from 'components/APP/App';
 import { Provider } from "react-redux"; // импрортировала и использовала Provider
 import store from "./redux/store"; // импортировала store
-import { Toaster } from "react-hot-toast"; //импортировала для всплывающих окон
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,8 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter basename="/the-DOMinators-frontend-project">
       {/* <BrowserRouter> */}
       <App />
-      {/* добавила для всплывающих окон */}
-      <Toaster />
     </BrowserRouter>
     </Provider>
   </React.StrictMode>
