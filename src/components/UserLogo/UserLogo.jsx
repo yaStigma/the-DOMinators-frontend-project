5787c363a4d74c8d46bddc80e1dd81467e1e41bc
Tanya Yenakieva <yastigma@gmail.com>
2024-12-17 16:19:20 +0200

    fix2

--- src/components/UserLogo/UserLogo.jsx
+++ src/components/UserLogo/UserLogo.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import css from './UserLogo.module.css';
-import { selectUserInfo } from '../../redux/user/selectors';
-import { fetchUser } from '../../redux/user/operations';
+// import React from 'react';
+// import { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import css from './UserLogo.module.css';
+// import { selectUserInfo } from '../../redux/user/selectors';
+// import { fetchUser } from '../../redux/user/operations';
 
 const UserLogo = ({ userId }) => {
 //   const dispatch = useDispatch();
