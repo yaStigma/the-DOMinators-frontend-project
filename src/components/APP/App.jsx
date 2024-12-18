6ebbb0c71b6e4259c70286566af7ac3942eb9931
Your NameNastya-20 <asyataa2016@gmail.com>
2024-12-18 15:15:17 +0200

    update App

--- src/components/APP/App.jsx
+++ src/components/APP/App.jsx
@@ -1,35 +1,79 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import SharedLayout from "../../components/SharedLayout/SharedLayout.jsx";
-// import PrivateRoute from "./PrivateRoute";
-// import RestrictedRoute from "./RestrictedRoute";
-import Loader from "../Loader/Loader.jsx"
+import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+import SharedLayout from "../SharedLayout/SharedLayout";
+import Loader from "../Loader/Loader";
+import PrivateRoute from "./PrivateRoute";
+import RestrictedRoute from "./RestrictedRoute";
+
+import { refreshUser } from "../../redux/auth/operations";
+import { selectIsRefreshing, selectIsLoggedIn } from "../../redux/auth/selectors";
 
 export default function App() {
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  const WelcomePage = lazy(() => import('../../pages/WelcomePage/WelcomePage'));
-  const SignupPage = lazy(() => import('../../pages/SignUpPage/SignUpPage'));
-  const SigninPage = lazy(() => import('../../pages/SignInPage/SignInPage'));
-  // const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));  //исправить путь на верный
-  const NotFoundPage = lazy(() => import('../../pages/NotFoundPage/NotFoundPage'));
-  return(
-<Suspense fallback={<Loader/>}>
-  <Routes>
-    <Route path="/" element={<SharedLayout />}>
-    <Route index element={<WelcomePage />} />
-    <Route path="welcome" element={<WelcomePage />} />
-      <Route path="signup" element={<SignupPage />} />
-      <Route path="signin" element={<SigninPage />} />
-      {/* <Route path="home" element={ <HomePage/> } /> */}
+  // Lazy-loaded pages
+  const WelcomePage = lazy(() => import("../../pages/WelcomePage/WelcomePage"));
+  const SignupPage = lazy(() => import("../../pages/SignUpPage/SignUpPage"));
+  const SigninPage = lazy(() => import("../../pages/SignInPage/SignInPage"));
+  const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+  const NotFoundPage = lazy(() => import("../../pages/NotFoundPage/NotFoundPage"));
 
-      {/* <Route path="signup" element={<RestrictedRoute component={<SignupPage />} redirectTo="/home" />} /> */}
-      {/* <Route path="signin" element={<RestrictedRoute component={<SigninPage />} redirectTo="/home" />} /> */}
-      {/* <Route path="home" element={ <PrivateRoute component={<HomePage/>} redirectTo="/signin" /> } /> */}
-        </Route>
-        <Route path="*"  element={<NotFoundPage />} />
-  </Routes>
-</Suspense>
-  )
-};
+  useEffect(() => {
+    const token = localStorage.getItem("persist:auth");
+    if (token) {
+      dispatch(refreshUser());
+    }
+  }, [dispatch]);
 
+  if (isRefreshing) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      {/* добавила для всплывающих окон (Надя) */}
+      <ToastContainer
+        toastClassName="toast-custom"
+        progressClassName="toast-custom-progress"
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
+      {/* Application Routes */}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={isLoggedIn ? <HomePage /> : <WelcomePage />} />
+            <Route
+              path="signup"
+              element={<RestrictedRoute component={<SignupPage />} redirectTo="/home" />}
+            />
+            <Route
+              path="signin"
+              element={<RestrictedRoute component={<SigninPage />} redirectTo="/home" />}
+            />
+            <Route
+              path="home"
+              element={<PrivateRoute component={<HomePage />} redirectTo="/signin" />}
+            />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
