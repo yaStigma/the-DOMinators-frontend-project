e80fb4a1adfd35ba29815cf08a4e770fa3d578fc
TarasLys <opros0609@gmail.com>
2024-12-21 02:07:01 +0200

    open modal add request

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -1,71 +1,57 @@
 import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
-export const createWaterRecord = (token, amount, time) => async dispatch => {
-  try {
-    const response = await axios.post(
-      'https://the-dominators-back-project.onrender.com/water-records',
-      {
-        amount,
-        date: new Date().toISOString().split('T')[0] + 'T' + time + ':00Z',
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    dispatch({
-      type: 'CREATE_WATER_RECORD_SUCCESS',
-      payload: response.data,
-    });
-    alert(response.data.message);
-  } catch (error) {
-    if (error.response && error.response.data) {
-      alert(error.response.data.message);
-      throw new Error(error.response.data.message);
-    } else {
-      alert('An error occurred while processing the request.');
-      throw new Error('An error occurred while processing the request.');
-    }
-    // dispatch({
-    //   type: 'CREATE_WATER_RECORD_FAIL',
-    //   payload: error.response ? error.response.data : { message: 'An error occurred' },
-    // });
-  }
+axios.defaults.baseURL = 'https://the-dominators-back-project.onrender.com';
+
+const setAuthHeader = (accessToken) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
 };
 
-// import axios from 'axios';
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
 
-// export const createWaterRecord = (token, amount, time) => async (dispatch) => {
-//   try {
-//     const response = await axios.post(
-//       'https://the-dominators-back-project.onrender.com/water-records',
-//       {
-//         amount,
-//         date: new Date().toISOString().split('T')[0] + 'T' + time + ':00Z',
-//       },
-//       {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//           'Content-Type': 'application/json',
-//         },
-//       }
-//     );
-//     dispatch({
-//       type: 'CREATE_WATER_RECORD_SUCCESS',
-//       payload: response.data,
-//     });
-//     alert(response.data.message);
-//   } catch (error) {
-//     if (error.response && error.response.data) {
-//       alert(error.response.data.message);
-//     } else {
-//       alert('An error occurred while processing the request.');
-//     }
-//     dispatch({
-//       type: 'CREATE_WATER_RECORD_FAIL',
-//       payload: error.response ? error.response.data : { message: 'An error occurred' },
-//     });
-//   }
-// };
+export const createWaterRecord = createAsyncThunk(
+  'water/createRecord',
+
+  async ({ accessToken, amount, time }, thunkAPI) => {
+    try {
+      setAuthHeader(accessToken);
+
+      const response = await axios.post('/water', {
+        amount,
+        date: new Date().toISOString().split('T')[0] + 'T' + time + ':00Z',
+      });
+
+      toast.success(response.data.message, {
+        duration: 4000,
+        position: 'top-right',
+      });
+
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        const { status, message, data } = error.response.data;
+        toast.error(`Error: ${message}`, {
+          duration: 4000,
+          position: 'top-right',
+        });
+
+        return thunkAPI.rejectWithValue({ status, message, data });
+      }
+
+      toast.error(`Error: ${error.message}`, {
+        duration: 4000,
+        position: 'top-right',
+      });
+
+      return thunkAPI.rejectWithValue({
+        status: null,
+        message: error.message,
+        data: null,
+      });
+    }
+  }
+);
+
