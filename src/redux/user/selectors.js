3d676802b5136906484fb1386c789e91e21c1a63
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2024-12-23 15:50:02 +0100

    edit water modal

--- src/redux/user/selectors.js
+++ src/redux/user/selectors.js
@@ -1,2 +1,3 @@
 export const selectUserInfo = state => state.userInfo.user;
 export const userId = (state) => state.auth.userId;
+export const selectDailyNormaInfo = state => state.userInfo.daylyNorm;
